0793a0999e9d370957793701e6edd2c261bf848b
SES <sesstigit@gmail.com>
2018-04-06 16:44:59 +0930

    Added license header to all source files

--- examples/androidCar/androidCar.ino
+++ examples/androidCar/androidCar.ino
@@ -1,3 +1,19 @@
+/**
+*    This file is part of AndroidCar.
+*
+*    AndroidCar is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    AndroidCar is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with AndroidCar.  If not, see <http://www.gnu.org/licenses/>.
+**/
 /* The running arduino sketch on the Android Autonomous Vehicle by Team Pegasus
 *
 * Description: The vehicle is based on an Arduino Mega, that is connected to various sensors
